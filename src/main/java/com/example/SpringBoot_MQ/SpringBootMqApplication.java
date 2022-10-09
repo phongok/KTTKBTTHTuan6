78864d59d4a0f668ef01319bd9230e526ada8330
phongok <phongdevgithub666666@gmail.com>
2022-10-09 16:35:51 +0700

    up phan MQ

---
+++ src/main/java/com/example/SpringBoot_MQ/SpringBootMqApplication.java
@@ -0,0 +1,15 @@
+package com.example.SpringBoot_MQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
+
+@SpringBootApplication
+@EnableJms
+public class SpringBootMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBoot_MQ/SpringBootMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_MQ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
